c14fd38a1116f30b0afeb63160dab720a11bb283
kmkinney <kevinmk712@gmail.com>
2023-02-06 21:30:57 -0700

    More zsh config

--- zsh/ohmyzsh.sh
+++
@@ -1,6 +0,0 @@
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# ZSH Plugins go here
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone --depth=1 https://github.com/paulirish/git-open.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/git-open
--- zsh/setup.sh
+++ zsh/setup.sh
@@ -1,3 +1,8 @@
+# /////////////////////////////
+# // Kevin ZSH Setup Script
+# /////////////////////////////
+
+# Install oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 # ZSH Plugins go here
@@ -5,6 +10,7 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$
 git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone --depth=1 https://github.com/paulirish/git-open.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/git-open
 
+# Remove any old config file
 rm -f $HOME/.zshrc
 
 [[ ! -f $HOME/.zshrc ]] && ln -s $HOME/.dotfiles/zsh/.zshrc $HOME/.zshrc
