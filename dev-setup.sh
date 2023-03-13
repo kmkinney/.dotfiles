a7a3f1dbb2d0e4862ebff8b3c91938afc724312c
kmkinney <kevinmk712@gmail.com>
2023-03-12 21:48:38 -0600

    Updates

--- dev-setup.sh
+++ dev-setup.sh
@@ -1,17 +1,17 @@
-echo "Installing docker"
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
+#echo "Installing docker"
+#curl -fsSL https://get.docker.com -o get-docker.sh
+#sudo sh get-docker.sh
 
-echo "Installing aws cli v2"
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
-rm -rf awscliv2.zip aws/
+#echo "Installing aws cli v2"
+#curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+#unzip awscliv2.zip
+#sudo ./aws/install
+#rm -rf awscliv2.zip aws/
 
 echo "Installing nvm"
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
-echo "Installing tfenv"
-mkdir $HOME/.tfenv
-git clone https://github.com/tfutils/tfenv.git $HOME/.tfenv
-ln -s $HOME/.tfenv/bin/* $HOME/.local/bin
+#echo "Installing tfenv"
+#mkdir $HOME/.tfenv
+#git clone https://github.com/tfutils/tfenv.git $HOME/.tfenv
+#ln -s $HOME/.tfenv/bin/* $HOME/.local/bin
--- setup.sh
+++ setup.sh
@@ -10,31 +10,31 @@ else
 fi
 
 # vim config
-if [ ! -d $HOME/.vim ]
-then
-    echo "Setting up vim"
-    [[ -d $HOME/.vim ]] || mkdir $HOME/.vim
-    [[ -d $HOME/.vim/bundle ]] || mkdir $HOME/.vim/bundle
-
-    ln -s $PWD/vim/.vimrc $HOME/.vimrc
-    ln -s $PWD/vim/.vim/*.vim $HOME/.vim/
-    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-    echo "Running plugin install"
-    vim +PluginInstall +qall
-else
-    echo "Vim config already found"
-fi
+#if [ ! -d $HOME/.vim ]
+#then
+    #echo "Setting up vim"
+    #[[ -d $HOME/.vim ]] || mkdir $HOME/.vim
+    #[[ -d $HOME/.vim/bundle ]] || mkdir $HOME/.vim/bundle
+#
+    #ln -s $PWD/vim/.vimrc $HOME/.vimrc
+    #ln -s $PWD/vim/.vim/*.vim $HOME/.vim/
+    #git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    #echo "Running plugin install"
+    #vim +PluginInstall +qall
+#else
+    #echo "Vim config already found"
+#fi
 
 # Neovim Setup
-if [ ! -f $HOME/.nvimrc ]
-then
-    echo "Setting up neovim"
-    ln -s $PWD/nvim/.nvimrc $HOME/.nvimrc
-    mkdir -p $HOME/.config/nvim
-    ln -s $PWD/nvim/init.vim $HOME/.config/nvim/init.vim
-else
-    echo "Neovim config already found"
-fi
+#if [ ! -f $HOME/.nvimrc ]
+#then
+    #echo "Setting up neovim"
+    #ln -s $PWD/nvim/.nvimrc $HOME/.nvimrc
+    #mkdir -p $HOME/.config/nvim
+    #ln -s $PWD/nvim/init.vim $HOME/.config/nvim/init.vim
+#else
+    #echo "Neovim config already found"
+#fi
 
 # Alacritty Setup
 if [ ! -d $HOME/.config/alacritty ]
