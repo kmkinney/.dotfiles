ff3fab3dd69145658ebb617107ba034bacf6e148
kmkinney <kevinmk712@gmail.com>
2023-04-11 16:19:27 -0600

    Macos updates:

---
+++ bash/bashsetup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ -f ~/.bashrc ]]; then
+    echo "Making backup"
+    mv ~/.bashrc ~/.bashrc.bak
+fi
+
+if [[ -f ~/.bash_aliases ]]; then
+    echo "Making backup"
+    mv ~/.bash_aliases ~/.bash_aliases.bak
+fi
+
+DOTFILES_DIR="$(git rev-parse --show-toplevel)"
+
+echo "Linking .bashrc"
+ln -s $DOTFILES_DIR/bash/bashrc.sh ~/.bashrc
+ln -s $DOTFILES_DIR/bash/bash_aliases.sh ~/.bash_aliases
