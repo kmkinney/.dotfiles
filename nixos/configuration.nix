28ca670aa7e82bec3a61a5d2dbe027560dadb6a9
kmkinney <kevinmk712@gmail.com>
2024-03-02 20:45:40 -0700

    Add more to the nixos config

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,6 +1,6 @@
 # Started from https://github.com/Misterio77/nix-starter-configs/blob/main/minimal/nixos/configuration.nix
 # NIXOS Root Config
-{...}: {
+{pkgs, ...}: {
   # Nixos configuration
   imports = [
     ./hardware-configuration.nix
@@ -19,16 +19,59 @@
 
   # System config
   networking.hostName = "kmkinney-nixos";
+  networking.networkmanager.enable = true;
+
   boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
   # TODO: Configure your system-wide user settings (groups, etc), add more users as needed.
   users.users.kevin = {
     initialPassword = "nix";
+    description = "kevin";
     isNormalUser = true;
     openssh.authorizedKeys.keys = [
       # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
     ];
     extraGroups = ["networkmanager" "wheel"];
+    packages = with pkgs; [
+      steam
+      firefox
+      zsh
+      kitty
+      neofetch
+    ];
+  };
+
+  # System Packages
+  environment.systemPackages = with pkgs; [
+    vim
+    git
+    curl
+    wget
+    htop
+    tmux
+    tree
+  ];
+
+  # Programs
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+  };
+
+  programs.hyprland = {
+    enable = true;
+  };
+
+  # GDM
+  services.xserver = {
+    enable = true;
+    layout = "us";
+    displayManager.gdm = {
+      enable = true;
+      wayland = true;
+    };
   };
 
   # This setups a SSH server. Very important if you're setting up a headless system.
