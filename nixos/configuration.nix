ec48ea9a882270e00be7591784f7e32803e1998c
kmkinney <kevinmk712@gmail.com>
2024-03-02 22:59:52 -0700

    Disable flakes for now

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -12,10 +12,10 @@
     };
   };
 
-  nix.settings = {
-    experimental-features = "nix-command flakes";
-    auto-optimise-store = true;
-  };
+  #  nix.settings = {
+  #    experimental-features = "nix-command flakes";
+  #    auto-optimise-store = true;
+  #  };
 
   # System config
   networking.hostName = "kmkinney-nixos";
