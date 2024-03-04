72712533311c895c1caef67a345816fce0b187ac
kmkinney <kevinmk712@gmail.com>
2024-03-03 22:57:55 -0700

    Adding rebuild

--- nixos/home-manager/rebuild.nix
+++ nixos/home-manager/rebuild.nix
@@ -2,7 +2,7 @@
 pkgs.writeShellScriptBin "rebuild" ''
   echo "Rebuilding..."
   pushd ~/.dotfiles/nixos/home-manager/
-  ${pkgs.neovim}/bin/nvim .
+  ${pkgs.neovim}/bin/nvim $(${pkgs.fd}/bin/fd . ~/.dotfiles/nixos/home-manager/ -t f -x ${pkgs.fzf}/bin/fzf --preview "bat --style=numbers,changes --color=always {}")
   ${pkgs.git}/bin/git commit -am "Rebuild"
   ${pkgs.home-manager}/bin/home-manager switch --flake .
   popd
