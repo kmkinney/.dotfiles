b5bf1cc6359baf7b5a0c703533f1e87026a63e8b
kmkinney <kevinmk712@gmail.com>
2024-03-03 23:02:26 -0700

    Update message

--- nixos/home-manager/dotfiles.nix
+++ nixos/home-manager/dotfiles.nix
@@ -3,7 +3,7 @@ pkgs.writeShellScriptBin "edit-dotfiles" ''
   echo "Rebuilding..."
   pushd ~/.dotfiles/nixos/home-manager/
   ${pkgs.neovim}/bin/nvim $(${pkgs.fd}/bin/fd . ~/.dotfiles/nixos/home-manager/ -t f | ${pkgs.fzf}/bin/fzf)
-  ${pkgs.git}/bin/git commit -am "Rebuild"
+  ${pkgs.git}/bin/git commit -am "Updated dotfiles"
   ${pkgs.home-manager}/bin/home-manager switch --flake .
   popd
 ''
