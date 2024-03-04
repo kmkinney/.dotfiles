1ca94fb9b81b8e6c26ad8605bf9c4881bc3fdd33
kmkinney <kevinmk712@gmail.com>
2024-03-03 23:00:44 -0700

    Change name

--- nixos/home-manager/dotfiles.nix
+++ nixos/home-manager/dotfiles.nix
@@ -1,5 +1,5 @@
 {pkgs, ...}:
-pkgs.writeShellScriptBin "dotfiles" ''
+pkgs.writeShellScriptBin "edit-dotfiles" ''
   echo "Rebuilding..."
   pushd ~/.dotfiles/nixos/home-manager/
   ${pkgs.neovim}/bin/nvim $(${pkgs.fd}/bin/fd . ~/.dotfiles/nixos/home-manager/ -t f | ${pkgs.fzf}/bin/fzf)
