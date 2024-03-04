aafdcc4199d51937e73f39747d75781205a95cb5
kmkinney <kevinmk712@gmail.com>
2024-03-03 22:35:53 -0700

    Cleanup

--- nixos/home-manager/home.nix
+++ nixos/home-manager/home.nix
@@ -1,14 +1,11 @@
-{
-  config,
-  pkgs,
-  ...
-}: {
+{pkgs, ...}: {
   home.username = "kevin";
   home.homeDirectory = "/Users/kevin";
 
   home.stateVersion = "23.11"; # Please read the comment before changing.
 
   home.packages = with pkgs; [
+    neovim
     git
     kitty
   ];
@@ -26,24 +23,5 @@
     ".tmux.conf".source = ./config/tmux.conf;
     ".p10k.zsh".source = ./config/p10k.zsh;
     ".config/nvim".source = ./config/nvim;
-  };
-
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. If you don't want to manage your shell through Home
-  # Manager then you have to manually source 'hm-session-vars.sh' located at
-  # either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/kevin/etc/profile.d/hm-session-vars.sh
-  #
-  home.sessionVariables = {
-    # EDITOR = "emacs";
   };
 }
