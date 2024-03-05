a44e6c9f216e8dbfc50e0d647dc84cee79bedcee
kmkinney <kevinmk712@gmail.com>
2024-03-05 01:56:30 +0000

    Sudo rebuild switch

--- nixos/nixos/nixconfig.nix
+++ nixos/nixos/nixconfig.nix
@@ -4,6 +4,6 @@ pkgs.writeShellScriptBin "nixconfig" ''
   pushd ~/.dotfiles/nixos/nixos/
   ${pkgs.neovim}/bin/nvim .
   ${pkgs.git}/bin/git commit -am "Updated nixos config"
-  ${pkgs.nixos-rebuild}/bin/nixos-rebuild switch --flake .
+  sudo ${pkgs.nixos-rebuild}/bin/nixos-rebuild switch --flake .
   popd
 ''
