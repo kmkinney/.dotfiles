cd02b16e0ec188ff600434568c77731899c3e945
kmkinney <kevinmk712@gmail.com>
2024-03-06 05:34:37 +0000

    (Nix update) Wed Mar  6 05:34:37 AM UTC 2024

--- nixos/home-manager/home.nix
+++ nixos/home-manager/home.nix
@@ -37,6 +37,7 @@
     ".p10k.zsh".source = ./dotfiles/p10k.zsh;
     ".config/hypr/hyprland.conf".source = ./dotfiles/hyprland.conf;
     ".config/hypr/hyprpaper.conf".source = ./dotfiles/hyprpaper.conf;
+    ".config/kitty/kitty.conf".source = ./dotfiles/kitty.conf;
     ".config/dunst/dunstrc".source = ./dotfiles/dunstrc.cfg;
     ".config/waybar/dotfiles".source = ./dotfiles/waybar.jsonc;
     ".config/waybar/style.css".source = ./dotfiles/waybar.css;
