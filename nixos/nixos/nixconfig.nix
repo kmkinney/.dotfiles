a1b082d73575f92ecbcb8c102aff420e6ac5f690
kmkinney <kevinmk712@gmail.com>
2024-03-06 04:21:45 +0000

    Updated nixos config

--- nixos/nixos/nixconfig.nix
+++ nixos/nixos/nixconfig.nix
@@ -1,10 +1,12 @@
 {pkgs, ...}:
 pkgs.writeShellScriptBin "nixconfig" ''
-  echo "Rebuilding..."
+  echo "Rebuilding nixos and home-manager configurations..."
   pushd ~/.dotfiles/nixos/
   ${pkgs.neovim}/bin/nvim .
-  ${pkgs.git}/bin/git commit -am "Updated nixos config"
+  ${pkgs.git}/bin/git commit -am "(Nix update) $(date)"
   sudo ${pkgs.nixos-rebuild}/bin/nixos-rebuild switch --flake .
-  ${pkgs.home-manager}/bin/home-manager switch --flake .
+  pushd ~/.dotfiles/nixos/home-manager/
+  ${pkgs.home-manager}/bin/home-manager switch --flake .#linux
+  popd
   popd
 ''
