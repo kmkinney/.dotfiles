f659d43787babe58c367e3babfa240e4b84b1588
kmkinney <kevinmk712@gmail.com>
2024-03-28 03:32:53 +0000

    Add pipewire config

---
+++ nixos/nixos/modules/pipewire.nix
@@ -0,0 +1,14 @@
+{...}: {
+  services = {
+    pipewire = {
+      enable = true;
+      audio.enable = true;
+      pulse.enable = true;
+      alsa = {
+        enable = true;
+        support32Bit = true;
+      };
+      jack.enable = true;
+    };
+  };
+}
