5e9853cd697d62e2314fd7bf96283580a5d9b43c
kmkinney <kevinmk712@gmail.com>
2024-07-21 21:17:31 +0000

    Can't set PS1 like this

--- home-manager/modules/terminal/bash.nix
+++ home-manager/modules/terminal/bash.nix
@@ -10,12 +10,12 @@
       py = "python3";
     };
     sessionVariables = {
-      PS1 = "\[\e[01;37m\]\u\[\e[01;34m\]@\h:\[\e[01;36m\]\w\[\e[01;33m\]\$\[\e[00m\]";
+      # PS1 = "\[\e[01;37m\]\u\[\e[01;34m\]@\h:\[\e[01;36m\]\w\[\e[01;33m\]\$\[\e[00m\]";
       MOZ_ENABLE_WAYLAND = 1;
       ANDROID_HOME = "~/Android/Sdk/";
     };
-    # bashrcExtra = ''
-    #   stty -ixon
-    # '';
+    bashrcExtra = ''
+      stty -ixon
+    '';
   };
 }
