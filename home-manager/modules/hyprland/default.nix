ae1426f6dc3f7d922ef63e6e556537771b8f361f
kmkinney <kevinmk712@gmail.com>
2024-07-21 23:05:15 +0000

    Try generating waybar config with nix

--- home-manager/modules/hyprland/default.nix
+++ home-manager/modules/hyprland/default.nix
@@ -6,6 +6,6 @@
   home.file.".config/hypr/hyprpaper.conf".source = ./hyprpaper.conf;
   home.file.".config/hypr/hyprlock.conf".source = ./hyprlock.conf;
   home.file.".config/dunst/dunstrc".source = ./dunstrc.cfg;
-  home.file.".config/waybar/config".source = ./waybar.jsonc;
+  # home.file.".config/waybar/config".source = ./waybar.jsonc;
   home.file.".config/waybar/style.css".source = ./waybar.css;
 }
