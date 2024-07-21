5cdc2c3abba7c6338ae6c13976bc539b344c8e10
kmkinney <kevinmk712@gmail.com>
2024-07-21 22:32:35 +0000

    Fix indent

--- home-manager/modules/terminal/zellij.nix
+++ home-manager/modules/terminal/zellij.nix
@@ -28,26 +28,26 @@
     # }
     # ''
     text = ''
-       keybinds {
-         unbind "Ctrl s"
-         unbind "Ctrl h"
-       }
-       theme "everforest-dark-medium"
-       themes {
-         everforest-dark-medium {
-      fg "#d3c6aa"
-      bg "#2f383e"
-      black "#272e33"
-      red "#d6494d"
-      green "#a7c080"
-      yellow "#dbbc7f"
-      blue "#7fbbb3"
-      magenta "#d699b6"
-      cyan "#83c092"
-      white "#d3c6aa"
-      orange "#e69875"
-         }
-       }
+      keybinds {
+        unbind "Ctrl s"
+        unbind "Ctrl h"
+      }
+      theme "everforest-dark-medium"
+      themes {
+        everforest-dark-medium {
+          fg "#d3c6aa"
+          bg "#2f383e"
+          black "#272e33"
+          red "#d6494d"
+          green "#a7c080"
+          yellow "#dbbc7f"
+          blue "#7fbbb3"
+          magenta "#d699b6"
+          cyan "#83c092"
+          white "#d3c6aa"
+          orange "#e69875"
+        }
+      }
     '';
   };
 }
