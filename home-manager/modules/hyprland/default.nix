051998e53fde14238b804e68dd673abb5f997e7d
kmkinney <kevinmk712@gmail.com>
2024-07-21 23:06:56 +0000

    Import module

--- home-manager/modules/hyprland/default.nix
+++ home-manager/modules/hyprland/default.nix
@@ -1,6 +1,7 @@
 # This module contains all of the configuration for hyprland and related utilities
 # This module requires hyprland module to be installed with nixos
 {...}: {
+  imports = [./waybar.nix];
   # Dotfiles for hyprland and related configs
   home.file.".config/hypr/hyprland.conf".source = ./hyprland.conf;
   home.file.".config/hypr/hyprpaper.conf".source = ./hyprpaper.conf;
