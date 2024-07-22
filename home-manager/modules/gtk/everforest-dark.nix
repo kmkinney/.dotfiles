6aeb05661daa9ecc84aa59cbeb44cdbc358283f3
kmkinney <kevinmk712@gmail.com>
2024-07-22 04:23:29 +0000

    Use different dir

--- home-manager/modules/gtk/everforest-dark.nix
+++ home-manager/modules/gtk/everforest-dark.nix
@@ -3,15 +3,15 @@ pkgs.stdenv.mkDerivation {
   name = "everforest-dark";
 
   src = pkgs.fetchurl {
-    url = "https://github.com/kmkinney/.dotfiles/blob/9a74f998f5a5b7fc10fdde1962cf37b414b14d68/home-manager/modules/gtk/Everforest-Dark-BL.zip";
-    sha256 = "sha256-V5426nC7f66AiQXgityYddyuvQa4mPdyBkKsH1PAUJM=";
+    url = "https://github.com/kmkinney/.dotfiles/raw/15612d55917d5214eecedf555c54b0cfc9b12bce/home-manager/modules/gtk/Everforest-Dark-BL.zip";
+    sha256 = "sha256-hhNfPBs2YYS/PpIxjt6MEeNyztT6eSt1Z1LksGSelcQ=";
     # sha256 = pkgs.lib.fakeSha256;
   };
 
   dontUnpack = true;
 
   installPhase = ''
-    mkdir -p $out
-    ${pkgs.unzip}/bin/unzip $src -d $out/
+    mkdir -p $out/share/themes
+    ${pkgs.unzip}/bin/unzip $src -d $out/share/themes/
   '';
 }
