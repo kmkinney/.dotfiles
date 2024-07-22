700115a28d632f81e3484accc0737c24325e48b0
kmkinney <kevinmk712@gmail.com>
2024-07-21 19:15:47 -0600

    Add home packages

--- home-manager/systems/macos.nix
+++ home-manager/systems/macos.nix
@@ -1,9 +1,19 @@
 # Which modules to use for the macos system
-{...}: {
+{pkgs, ...}: {
   imports = [
     ../modules/neovim.nix
     ../modules/terminal
     ../modules/git.nix
     ../modules/vscode.nix
+  ];
+
+  home.packages = with pkgs; [
+    alejandra
+    pscale
+    ripgrep
+    (pkgs.nerdfonts.override {fonts = ["JetBrainsMono"];})
+
+    nodejs_20
+    corepack
   ];
 }
