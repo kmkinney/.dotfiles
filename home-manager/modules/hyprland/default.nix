a490de961a976f38591e0b3617e584709a379927
kmkinney <kevinmk712@gmail.com>
2024-07-22 00:48:20 +0000

    Cleanup hypr

--- home-manager/modules/hyprland/default.nix
+++ home-manager/modules/hyprland/default.nix
@@ -4,9 +4,7 @@
   imports = [./waybar.nix ./hyprpaper.nix];
   # Dotfiles for hyprland and related configs
   home.file.".config/hypr/hyprland.conf".source = ./hyprland.conf;
-  # home.file.".config/hypr/hyprpaper.conf".source = ./hyprpaper.conf;
   home.file.".config/hypr/hyprlock.conf".source = ./hyprlock.conf;
   home.file.".config/dunst/dunstrc".source = ./dunstrc.cfg;
-  # home.file.".config/waybar/config".source = ./waybar.jsonc;
   home.file.".config/waybar/style.css".source = ./waybar.css;
 }
