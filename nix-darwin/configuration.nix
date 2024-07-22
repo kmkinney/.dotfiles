22f380eeaccba02dc4568a9f70cde9932f23a4ae
kmkinney <kevinmk712@gmail.com>
2024-07-22 01:07:07 +0000

    Add nix darwin config

---
+++ nix-darwin/configuration.nix
@@ -0,0 +1,52 @@
+{pkgs, ...}: {
+  users.users.kevin = {
+    name = "kevin";
+    home = "/Users/kevin";
+  };
+  environment.systemPackages = with pkgs; [
+    vim
+    tmux
+    kitty
+    slack
+  ];
+  services.nix-daemon.enable = true;
+  nix.settings.experimental-features = "nix-command flakes";
+
+  nix.extraOptions = ''
+    extra-platforms = aarch64-darwin x86_64-darwin
+    experimental-features = nix-command flakes
+  '';
+  programs.zsh.enable = true;
+  system.stateVersion = 4;
+  nixpkgs.hostPlatform = "aarch64-darwin";
+  nixpkgs.config.allowUnfree = true;
+
+  # System settings
+  system.keyboard = {
+    enableKeyMapping = true;
+    remapCapsLockToEscape = true;
+  };
+  system.startup.chime = false;
+  system.defaults.dock.autohide = true;
+  system.defaults.finder = {
+    AppleShowAllFiles = true;
+  };
+  # system.defaults.universalaccess.reduceMotion = true;
+  system.defaults.NSGlobalDomain = {
+    AppleShowAllFiles = true;
+    # InitialKeyRepeat = 0;
+    # KeyRepeat = 0;
+  };
+
+  homebrew = {
+    enable = true;
+    brews = [
+      "gnu-sed"
+      "nixpacks"
+    ];
+    casks = [
+      "nordpass"
+      "steam"
+    ];
+  };
+}
---
+++ nix-darwin/flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Macos Nix Configuration";
+
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = {
+    nix-darwin,
+    home-manager,
+    ...
+  }: {
+    darwinConfigurations = {
+      "Kevins-MacBook-Pro" = nix-darwin.lib.darwinSystem {
+        system = "aarch64-darwin";
+        modules = [
+          ./configuration.nix
+          home-manager.darwinModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.kevin = import ./home.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass
+            # arguments to home.nix
+          }
+        ];
+      };
+    };
+  };
+}
