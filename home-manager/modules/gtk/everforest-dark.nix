7daafe6337c54929ed6aa06e8daa6f6779de8415
kmkinney <kevinmk712@gmail.com>
2024-07-22 03:57:24 +0000

    fake sha 256

--- home-manager/modules/gtk/everforest-dark.nix
+++ home-manager/modules/gtk/everforest-dark.nix
@@ -6,7 +6,7 @@ pkgs.stdenv.mkDerivation {
     owner = "Fausto-Korpsvart";
     repo = "Everforest-GTK-Theme";
     rev = "b3af23bc47ce7a15487eb273b12a5dc2a3566621";
-    sha256 = "fake";
+    sha256 = pkgs.lib.fakeSha256;
   };
 
   installPhase = ''
