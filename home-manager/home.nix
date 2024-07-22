d7d3a09a4c32cac7ad76c45b0a9baf4fb4f80d7b
kmkinney <kevinmk712@gmail.com>
2024-07-21 19:48:06 -0600

    Use variable

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -14,13 +14,13 @@
 in {
   nixpkgs.config.allowUnfree = true;
   home.username = "kevin";
-  home.homeDirectory = homeDirectory.system;
+  home.homeDirectory = homeDirectory.${system};
 
   home.stateVersion = "23.11"; # Please read the comment before changing.
 
   programs.home-manager.enable = true;
 
-  imports = systemsModule.system or [];
+  imports = systemsModule.${system} or [];
 
   # TODO: figure this out with mkEnable
   # systems.linux.enable = system == "x86_64-linux";
