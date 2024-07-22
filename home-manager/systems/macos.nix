ccacd4feb1e5b4f6f8c6397d2ba57222d0b5093f
kmkinney <kevinmk712@gmail.com>
2024-07-21 19:14:26 -0600

    Fix path

--- home-manager/systems/macos.nix
+++ home-manager/systems/macos.nix
@@ -1,9 +1,9 @@
 # Which modules to use for the macos system
 {...}: {
   imports = [
-    ./modules/neovim.nix
-    ./modules/terminal
-    ./modules/git.nix
-    ./modules/vscode.nix
+    ../modules/neovim.nix
+    ../modules/terminal
+    ../modules/git.nix
+    ../modules/vscode.nix
   ];
 }
