7d87f2fa19978cc79905e5ece50d336e452afb43
Kevin <kevinmk712@gmail.com>
2024-07-24 22:15:43 -0600

    Add more steam options

--- nixos/modules/gaming.nix
+++ nixos/modules/gaming.nix
@@ -1,7 +1,22 @@
-{...}: {
+{pkgs, ...}: {
   programs.steam = {
     enable = true;
+    gamescopeSession = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
   };
+  programs.gamemode.enable = true;
+
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  services.xserver.videoDrivers = ["amdgpu"];
+
+  environment.systemPackages = with pkgs; [
+    mangohud
+    protonup
+  ];
 }
