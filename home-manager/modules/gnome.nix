f405a3a35bccfb0772de29df2a620b9d38f82c39
Kevin <kevinmk712@gmail.com>
2024-07-25 21:36:11 -0600

    Get rid of desktop icons ext

--- home-manager/modules/gnome.nix
+++ home-manager/modules/gnome.nix
@@ -1,16 +1,13 @@
 {pkgs, ...}: {
   home.packages = with pkgs.gnomeExtensions; [
     dash-to-dock
-    desktop-icons-ng-ding
   ];
   dconf = {
     enable = true;
     settings."org/gnome/shell" = {
       disable-user-extensions = false;
       enabled-extensions = with pkgs.gnomeExtensions; [
-        # dock-from-dash.extensionUuid
         dash-to-dock.extensionUuid
-        desktop-icons-ng-ding.extensionUuid
       ];
     };
     settings."org/gnome/shell/extensions/dash-to-dock" = {
