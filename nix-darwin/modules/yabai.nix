dd4f331dc0ef0bb71cfc3b40e8ac5172abb86466
kmkinney <kevinmk712@gmail.com>
2024-07-30 09:49:35 -0600

    Update skhd

--- nix-darwin/modules/yabai.nix
+++ nix-darwin/modules/yabai.nix
@@ -10,12 +10,12 @@
   services.skhd = {
     enable = true;
     skhdConfig = ''
-      # cmd - j : yabai -m window --focus south
-      # cmd - k : yabai -m window --focus north
-      # cmd - h : yabai -m window --focus west
-      # cmd - l : yabai -m window --focus east
+      cmd - j : yabai -m window --focus south
+      cmd - k : yabai -m window --focus north
+      cmd - h : yabai -m window --focus west
+      cmd - l : yabai -m window --focus east
 
-      cmd - return: /etc/profiles/per-user/kevin/bin/kitty
+      # cmd - return: /etc/profiles/per-user/kevin/bin/kitty
     '';
   };
 }
