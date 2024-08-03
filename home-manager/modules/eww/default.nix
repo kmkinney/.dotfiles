ce8d35cc2d0dbea6dbbe2abd9dc566b675d2f9dd
Kevin <kevinmk712@gmail.com>
2024-08-03 17:23:10 -0600

    Move to nested config folder

--- home-manager/modules/eww/default.nix
+++ home-manager/modules/eww/default.nix
@@ -1,8 +1,9 @@
 {config, ...}: let
-  configDir = "${config.home.homeDirectory}/.dotfiles/home-manager/modules/eww";
+  # configDir = "${config.home.homeDirectory}/.dotfiles/home-manager/modules/eww";
+  configDir = "./modules/eww";
 in {
   programs.eww = {
     enable = true;
-    configDir = configDir;
+    configDir = ./config;
   };
 }
