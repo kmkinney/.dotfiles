2530fd4653ca637a9f01be24f43746f1644c060b
Kevin <kevinmk712@gmail.com>
2024-08-03 16:27:00 -0600

    Fixing some more hyprland config

--- home-manager/modules/hyprland/hyprpaper.nix
+++ home-manager/modules/hyprland/hyprpaper.nix
@@ -8,11 +8,7 @@ in {
       splash = false;
       preload = ["${cabin}"];
       wallpaper = [
-        "DP-4,${cabin}"
-        "DP-3,${cabin}"
-        "DP-2,${cabin}"
-        "DP-1,${cabin}"
-        "eDP-1,${cabin}"
+        ",${cabin}"
       ];
     };
   };
