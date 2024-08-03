6ca44f37865384c09717a29b659aaf321463e4f4
Kevin <kevinmk712@gmail.com>
2024-08-03 16:07:17 -0600

    Try to config wezterm
    Not all is working, everforest is only on nightly

--- home-manager/modules/terminal/wezterm.nix
+++ home-manager/modules/terminal/wezterm.nix
@@ -2,6 +2,18 @@
   programs = {
     wezterm = {
       enable = true;
+      extraConfig =
+        /*
+        lua
+        */
+        ''
+          return {
+            font = wezterm.font("JetBrains Mono"),
+            color_scheme = 'Everforest Dark Hard (Gogh)',
+            enable_tab_bar = false,
+            enable_wayland = true
+          }
+        '';
     };
   };
 }
