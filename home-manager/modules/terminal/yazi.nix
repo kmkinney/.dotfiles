255d339a00f505e05c70d7ffbb77675f76b9c8a7
Kevin <kevinmk712@gmail.com>
2024-08-03 15:40:15 -0600

    Add yazi module

---
+++ home-manager/modules/terminal/yazi.nix
@@ -0,0 +1,13 @@
+# Terminal file browsing
+{...}: {
+  programs = {
+    yazi = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+    fd.enable = true;
+    fzf.enable = true;
+    rg.enable = true;
+    jq.enable = true;
+  };
+}
