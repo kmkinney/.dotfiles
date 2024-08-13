d986687b2f22e78333fc54fbfa0cad3c92f4cb94
Kevin <kevinmk712@gmail.com>
2024-08-12 18:08:51 -0600

    Don't show dock for now

--- home-manager/modules/ags/config/config.js
+++ home-manager/modules/ags/config/config.js
@@ -1,10 +1,14 @@
 import { applauncher } from "./widgets/applications.js";
-import { Dock } from "./widgets/dock.js";
+// import { Dock } from "./widgets/dock.js";
 import { Bar } from "./widgets/bar.js";
 
 App.config({
   style: "./style.css",
-  windows: [Bar(0), Dock(), applauncher],
+  windows: [
+    Bar(0),
+    // Dock(),
+    applauncher,
+  ],
 });
 
 export {};
