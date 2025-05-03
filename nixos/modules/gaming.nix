c333b6e86f4e0370a2d31b93a6659949d3b0d1df
Kevin <kevinmk712@gmail.com>
2025-05-03 09:51:39 -0600

    Add java 17 and gba emu

--- nixos/modules/gaming.nix
+++ nixos/modules/gaming.nix
@@ -31,6 +31,8 @@
     fuse
     fuse3
     appimage-run
+    jdk17
+    mgba
   ];
   environment.sessionVariables = {
     STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\${HOME}/.steam/root/compatibilitytools.d";
