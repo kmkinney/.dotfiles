ae08a55aced01c2faab6e477f0de4cf5a69be36c
Kevin <kevinmk712@gmail.com>
2025-05-03 09:59:23 -0600

    Add auto gc

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -24,9 +24,16 @@
     };
   };
 
-  nix.settings = {
-    experimental-features = "nix-command flakes";
-    auto-optimise-store = true;
+  nix = {
+    settings = {
+      experimental-features = "nix-command flakes";
+      auto-optimise-store = true;
+    };
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 14d";
+    };
   };
 
   boot.loader.systemd-boot.enable = true;
