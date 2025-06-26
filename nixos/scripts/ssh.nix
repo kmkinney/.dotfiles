06c7e387079a215cd39d53213f02df567e5dff66
kmkinney <kevinmk712@gmail.com>
2025-06-26 08:02:53 -0600

    Update

--- nixos/scripts/ssh.nix
+++ nixos/scripts/ssh.nix
@@ -4,6 +4,7 @@
 }:
 pkgs.writeShellScriptBin "ssh-init" ''
   echo "Generating ssh keys and adding to agent"
+  [ -f $HOME/.ssh/id_ed25519 ] && echo "File already exists" && exit 1;
   pushd $HOME/.ssh
   ${pkgs.openssh}/bin/ssh-keygen -t ed25519 -C "kevinmk712@gmail.com" -N "" -f $HOME/.ssh/id_ed25519
   eval "$(${pkgs.openssh}/bin/ssh-agent -s)"
