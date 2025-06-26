ae3367196ede32f3380c4d9d9e3ef623c7b2b493
kmkinney <kevinmk712@gmail.com>
2025-06-26 07:55:58 -0600

    Updates to hyprland config

--- home-manager/modules/hyprland/hyprland.nix
+++ home-manager/modules/hyprland/hyprland.nix
@@ -146,10 +146,10 @@ in {
             passes = 1
           }
 
-          drop_shadow = yes
-          shadow_range = 4
-          shadow_render_power = 3
-          col.shadow = rgba(1a1a1aee)
+    #drop_shadow = yes
+    #shadow_range = 4
+    #shadow_render_power = 3
+    #col.shadow = rgba(1a1a1aee)
       }
 
       animations {
@@ -174,7 +174,7 @@ in {
 
       master {
           # See https://wiki.hyprland.org/Configuring/Master-Layout/ for more
-          new_is_master = true
+    #new_is_master = true
       }
 
       gestures {
