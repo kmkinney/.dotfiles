e3f3f401e7530df236d09ed384dbf155370a3115
kmkinney <kevinmk712@gmail.com>
2025-06-26 08:08:31 -0600

    Updates

--- nixos/modules/hyprland.nix
+++ nixos/modules/hyprland.nix
@@ -1,6 +1,12 @@
-{pkgs, ...}: {
+{inputs, pkgs, ...}: let
+  hyprlandPkgs = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system};
+in {
   programs.hyprland = {
-    enable = true;
+    enable = true; 
+    # set the flake package
+    package = hyprlandPkgs.hyprland;
+    # make sure to also set the portal package, so that they are in sync
+    portalPackage = hyprlandPkgs.xdg-desktop-portal-hyprland;
   };
 
   # GDM
@@ -23,4 +29,11 @@
     wofi
     wl-clipboard
   ];
+
+  # Cached builds, from https://wiki.hypr.land/Nix/Cachix/
+  nix.settings = {
+    substituters = ["https://hyprland.cachix.org"];
+    trusted-substituters = ["https://hyprland.cachix.org"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  };
 }
