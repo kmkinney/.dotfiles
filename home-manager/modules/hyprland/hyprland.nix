2cfe71e7cceb517c6d861d8f9046ec5d276ce688
kmkinney <kevinmk712@gmail.com>
2025-07-31 11:24:29 -0600

    Bigger

--- home-manager/modules/hyprland/hyprland.nix
+++ home-manager/modules/hyprland/hyprland.nix
@@ -29,7 +29,7 @@ in {
         "desc:LG Electronics LG TV SSCR2 0x01010101,4096x2160,0x0,2"
         "desc:LG Electronics LG Ultra HD 0x00027BFE,1920x1080,0x0,1"
         # Work monitor
-        "desc:LG Electronics LG HDR 4K 305NTABE1376,preferred,0x0,1.5"
+        "desc:LG Electronics LG HDR 4K 305NTABE1376,preferred,0x0,1.75"
       ];
 
       # Startup
