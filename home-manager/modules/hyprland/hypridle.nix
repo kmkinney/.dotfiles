d0112bb1ae50f604d1116994ab8e1c5fc686dbcb
kmkinney <kevinmk712@gmail.com>
2025-07-31 21:41:24 -0600

    Fix hypridle

--- home-manager/modules/hyprland/hypridle.nix
+++ home-manager/modules/hyprland/hypridle.nix
@@ -1,4 +1,11 @@
-{...}: {
+{pkgs, ...}: {
+  # Ensure packages are available in the system environment
+  environment.systemPackages = with pkgs; [
+    hypridle
+    swaylock-fancy
+    brightnessctl
+  ];
+
   services.hypridle = {
     enable = true;
 
@@ -12,12 +19,23 @@
 
       listener = [
         {
-          timeout = 1;
-          on-timeout = "dunstify 'Test (1 seconds)'";
+          timeout = 180;
+          on-timeout = "brightnessctl -s set 15%";
+          on-resume = "brightnessctl -r";
         }
         {
           timeout = 300;
           on-timeout = "loginctl lock-session";
+        }
+        {
+          timeout = 350;
+          on-timeout = "hyprctl dispatch dpms off";
+          on-resume = "hyprctl dispatch dpms on";
+        }
+
+        {
+          timeout = 420;
+          on-timeout = "systemctl suspend";
         }
       ];
     };
--- home-manager/modules/hyprland/hyprland.nix
+++ home-manager/modules/hyprland/hyprland.nix
@@ -39,6 +39,7 @@ in {
       exec-once = [
         "dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
         "dunst"
+        "hypridle"
       ];
 
       exec = [
