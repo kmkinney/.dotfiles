1fc6fd4509e92bd0dc3d5e0578fd8c8199ccc356
kmkinney <kevinmk712@gmail.com>
2025-08-08 21:51:00 -0600

    Starting some stuff

--- home-manager/modules/neovim.nix
+++ home-manager/modules/neovim.nix
@@ -1,9 +1,18 @@
-{config, ...}: let
+{
+  config,
+  pkgs,
+  ...
+}: let
   configDir = "${config.home.homeDirectory}/.dotfiles";
   nvimDir = "${configDir}/home-manager/dotfiles/nvim";
 in {
   programs.neovim.enable = true;
   programs.neovim.defaultEditor = true;
+
+  home.packages = with pkgs; [
+    # LSPs
+    lua-language-server
+  ];
 
   home.file = {
     # This is to allow lazy-lock.json to be writable
