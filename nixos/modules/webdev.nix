2ef56841fa4c34f5a0508c7caba4ddaefd15a42b
kmkinney <kevinmk712@gmail.com>
2025-10-16 14:30:58 -0600

    FIxes

--- nixos/modules/webdev.nix
+++ nixos/modules/webdev.nix
@@ -1,10 +1,5 @@
-{
-  pkgs,
-  inputs,
-  ...
-}: let
-  pkgs-playwright = import inputs.nixpkgs-playwright {system = pkgs.stdenv.system;};
-  browsers = (builtins.fromJSON (builtins.readFile "${pkgs-playwright.playwright-driver}/browsers.json")).browsers;
+{pkgs, ...}: let
+  browsers = (builtins.fromJSON (builtins.readFile "${pkgs.playwright-driver}/browsers.json")).browsers;
   chromium-rev = (builtins.head (builtins.filter (x: x.name == "chromium") browsers)).revision;
 in {
   environment.systemPackages = with pkgs; [
@@ -29,7 +24,7 @@ in {
   environment.variables.PRISMA_SCHEMA_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/schema-engine";
   environment.variables.PLAYWRIGHT_BROWSERS_PATH = pkgs.playwright-driver.browsers;
   environment.variables.PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS = "true";
-  environment.variables.PLAYWRIGHT_LAUNCH_OPTIONS_EXECUTABLE_PATH = "${pkgs-playwright.playwright.browsers}/chromium-${chromium-rev}/chrome-linux/chrome";
+  environment.variables.PLAYWRIGHT_LAUNCH_OPTIONS_EXECUTABLE_PATH = "${pkgs.playwright-drvier.browsers}/chromium-${chromium-rev}/chrome-linux/chrome";
 
   # Puppeteer
   environment.variables.PUPPETEER_EXECUTABLE_PATH = "${pkgs.chromium}/bin/chromium";
