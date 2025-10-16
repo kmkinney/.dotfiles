688c0131bff53f03c50f4a465d324a5eb5aaf151
kmkinney <kevinmk712@gmail.com>
2025-10-16 14:14:09 -0600

    string env var

--- nixos/modules/webdev.nix
+++ nixos/modules/webdev.nix
@@ -20,7 +20,7 @@
   environment.variables.PRISMA_QUERY_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/query-engine";
   environment.variables.PRISMA_SCHEMA_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/schema-engine";
   environment.variables.PLAYWRIGHT_BROWSERS_PATH = pkgs.playwright-driver.browsers;
-  environment.variables.PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS = true;
+  environment.variables.PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS = "true";
 
   # Puppeteer
   environment.variables.PUPPETEER_EXECUTABLE_PATH = "${pkgs.chromium}/bin/chromium";
